f90a83df9a023764e9f452eca9b899488b270c7f
Jeff Lau <jeff-lau@live.com>
2017-06-12 14:45:33 +0800

    refactor

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-export let db,
-export let render,
+export let db
+export let render
        let middleware
 
-export function update (newData) {
+export function update(newData){
   let data = this.middleware.reduce((state, middleware) => middleware(state), newData)
   db = data;
   render()
 }
 
-export default function redaxeInit (initialData, renderer, middleware) {
+export default function redaxeInit(initialData, renderer, middleware){
   db = initialData
   render = renderer
   middleware = middleware
